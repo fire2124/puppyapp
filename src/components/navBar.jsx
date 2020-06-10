8a7f7606f10f4bf8599b25a26f18c9dc0b87f52a
fire2124 <filip.reichl@gmail.com>
2020-06-10 13:03:45 +0200

    update navbar and landing page

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import { AuthorizationContext } from "../authorizationProvider";
+import ilustracia from "../staticImage/logo-odtlacok.png";
 
 const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">
+      {/* <Link className="navbar-brand" to="/">
         PuppyApp
-      </Link>
-      <button
+      </Link> */}
+      {/* <button
         className="navbar-toggler"
         type="button"
         data-toggle="collapse"
@@ -17,39 +18,79 @@ const NavBar = () => {
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span className="navbar-toggler-icon" />
-      </button>
+        <span className="navbar-toggler-icon" /> flex justify-start bg-orange-200 relative 
+        //container flex flex-wrap items-center justify-between flex-wrap bg-teal-500 p-6
+      </button> */}
       <AuthorizationContext.Consumer>
-      {({loggedIn, userFirstName}) => (
-              <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div className="navbar-nav">
-                {loggedIn !== true && (
-                  <React.Fragment>
-                    <NavLink className="nav-item nav-link" to="/registerShelter">
+        {({ loggedIn, userFirstName }) => (
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              {loggedIn !== true && (
+                <React.Fragment className="py-6">
+                  <div className=" md:flex md:items-center md:justify-start mx-auto  py-5 px-5 ">
+                    <img src={ilustracia} alt="Logo" width="30px" />
+                  </div>
+                  <div className=" md:flex md:items-center md:justify-center mx-auto ">
+                    <NavLink
+                      className=" text-black text-center bg-orange-400 px-4 py-2 m-2 rounded-full py-2 px-4 py-2 px-4"
+                      to="/"
+                    >
+                      Hľadaj psíka
+                    </NavLink>
+                    <NavLink
+                      className=" text-black text-center bg-orange-400 px-4 py-2 m-2 rounded-full py-2 px-4 py-2 px-4"
+                      to="/"
+                    >
+                      Útulky
+                    </NavLink>
+                    <NavLink
+                      className="  text-black text-center bg-orange-400 px-4 py-2 m-2 rounded-full py-2 px-4 py-2 px-4"
+                      to="/"
+                    >
+                      „HAU TU”
+                    </NavLink>
+                  </div>
+                  <div className=" md:flex md:items-center md:justify-end mx-auto ">
+                    <NavLink
+                      className=" text-white font-semibold text-center bg-orange px-4 py-2 m-2 rounded-full py-2 px-4 "
+                      to="/registerShelter"
+                    >
                       Registrácia útulku
                     </NavLink>
-                    <NavLink className="nav-item nav-link" to="/login">
+                    <NavLink
+                      className="  text-orange font-semibold text-center bg-orange-400 px-4 py-2 m-2 rounded-full py-2 px-4 py-2 px-4 border-2 border-orange content-end"
+                      to="/login"
+                    >
                       Prihlásanie
                     </NavLink>
-                    <NavLink className="nav-item nav-link" to="/registerUser">
+                    <NavLink
+                      className="text-white font-semibold text-center bg-orange px-4 py-2 m-2 rounded-full py-2 px-4 "
+                      to="/registerUser"
+                    >
                       Registrácia používateľa
                     </NavLink>
-                  </React.Fragment>
-                )}
-                {loggedIn === true && (
-                  <React.Fragment>
-                    <NavLink className="nav-item nav-link" to="/userProfile">
-                      {userFirstName} Profil
-                    </NavLink>
-                    <NavLink className="nav-item nav-link" to="/logout">
-                      Odhlásiť
-                    </NavLink>
-                  </React.Fragment>
-                )}
-              </div>
+                  </div>
+                </React.Fragment>
+              )}
+              {loggedIn === true && (
+                <React.Fragment className="flex justify-start bg-orange-200">
+                  <NavLink
+                    className="text-orange-700 text-center bg-orange-400 px-4 py-2 m-2"
+                    to="/userProfile"
+                  >
+                    {userFirstName} Profil
+                  </NavLink>
+                  <NavLink
+                    className="text-orange-700 text-center bg-orange-400 px-4 py-2 m-2"
+                    to="/logout"
+                  >
+                    Odhlásiť
+                  </NavLink>
+                </React.Fragment>
+              )}
             </div>
-      )}
-
+          </div>
+        )}
       </AuthorizationContext.Consumer>
     </nav>
   );
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,25 +1,76 @@
 import React, { Component } from "react";
-import ilustracia from "../staticImage/Ilustracia_backgroundless.png"
+import ilustracia from "../staticImage/Ilustracia_backgroundless.png";
+import ilustracia2 from "../staticImage/Ilustracia_backgroundless2.png";
+import ilustracia3 from "../staticImage/Ilustracia_backgroundless3.png";
 
 class Landing extends Component {
-
   render() {
     return (
       <div>
         <div className="container mx-auto">
-        <div className="flex flex-no-wrap mx-auto">
-          <div className="flex-none w-6/12 m-auto">
-            <h1 className="text-left text-4xl font-bold">Zoznámte sa so svojím<br/> ideálnym psím spoločníkom</h1>
-            <p className="text-lg">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
-            <button className="text-orange p-2 bg-regal-blue">Nájsť psíka</button>
+          <div className="flex flex-no-wrap mx-auto">
+            <div className="flex-none w-6/12 m-auto">
+              <h1 className="text-left text-4xl font-bold text-orange">
+                Zoznámte sa so svojím
+                <br /> ideálnym psím spoločníkom
+              </h1>
+              <p className="text-lg">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled
+              </p>
+              <button className="text-white font-semibold text-center bg-orange px-8 py-2 m-2 rounded-full py-2 px-8">
+                Nájsť psíka
+              </button>
+            </div>
+            <div className="flex-none w-6/12 mx-auto">
+              <img src={ilustracia} alt="Logo" />
+            </div>
           </div>
-          <div className="flex-none w-6/12 mx-auto">
-            <img src={ilustracia} alt="Logo" />
+          <div className="flex flex-no-wrap mx-auto">
+            <div className="flex-none w-6/12 m-auto">
+              <img src={ilustracia2} alt="Logo" />
+            </div>
+            <div className="flex-none w-6/12 mx-auto ">
+              <h1 className="text-left text-4xl font-bold text-orange">
+               HAU TU ? (blog)
+              </h1>
+              <p className="text-lg ">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled
+              </p>
+              <button className="text-white font-semibold text-center bg-orange px-8 py-2 m-2 rounded-full py-2 px-8">
+                Viac
+              </button>
+            </div>
           </div>
-        </div>
+          <div className="flex flex-no-wrap mx-auto">
+            <div className="flex-none w-6/12 m-auto">
+              <h1 className="text-left text-4xl font-bold text-orange">
+                Finančná pomoc
+              </h1>
+
+              <p className="text-lg">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled
+              </p>
+              <button className="text-white font-semibold text-center bg-orange px-8 py-2 m-2 rounded-full py-2 px-8">
+                Viac
+              </button>
+            </div>
+            <div className="flex-none w-6/12 mx-auto">
+              <img src={ilustracia3} alt="Logo" />
+            </div>
+          </div>
         </div>
       </div>
-    )}
+    );
   }
+}
 
 export default Landing;

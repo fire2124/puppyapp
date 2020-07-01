ba9ef569280bfee2733dc788be0c402af1cdf9d8
fire2124 <filip.reichl@gmail.com>
2020-07-01 14:10:45 +0200

    add register and login popups

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -4,21 +4,51 @@ import { Form } from "react-final-form";
 import FormTextField from "../components/common/formTextField";
 import { Link } from "react-router-dom";
 import ilustracia from "../staticImage/illustrations/Ilustracia_backgroundless.png";
+import Popup from "reactjs-popup";
+import { NavLink } from "react-router-dom";
+import authService from "../services/authService";
 
 class Login extends Component {
-  
-  onSubmit = async values => {
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+  openModal() {
+    this.setState({ open: true });
+  }
+  closeModal() {
+    this.setState({ open: false });
+  }
+
+  onSubmitForgotPassword = async (values) => {
+    console.log(values);
+
     try {
-      let response = await this.context.login(values);
-      if(response){
+      let response = await authService.forgotPassword(values);
+      console.log(response);
+
+      if (response) {
         this.props.history.push("/");
       }
     } catch (error) {
       console.log(error);
-      
     }
-    
-  }
+  };
+
+  onSubmit = async (values) => {
+    try {
+      let response = await this.context.login(values);
+      console.log(response);
+
+      if (response) {
+        this.props.history.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   render() {
     return (
@@ -32,7 +62,7 @@ class Login extends Component {
                   <h1 className="text-left text-4xl font-bold pod mr-40">
                     Prihlásiť sa
                   </h1>
-                  <p className="text-base mr-40">
+                  <p className="text-base mr-40 my-8">
                     Prihláste sa do svojho účtu pre uľahčenie <br />
                     hľadania Vášho domáceho miláčika!
                   </p>
@@ -54,37 +84,94 @@ class Login extends Component {
                       className="bg-transparent  w-full text-gray-700 mr-3 py-1 px-2 focus:outline-none"
                     />
                   </div>
-                
-                <Link
-                  className="text-orange font-semibold text-center bg-orange-400 px-4 py-2 m-2 rounded-full py-2 px-4 py-2 px-4 border-2 border-orange content-end my-8"
-                  to="/"
-                >
-                  Späť
-                </Link>
-                <button
-                  className="text-white font-semibold text-center bg-orange px-4 py-2 m-2 rounded-full py-2 px-4 my-8"
-                  type="submit"
-                  disabled={submitting || pristine}
-                >
-                  Prihlásiť
-                </button>
-                <div class="no-underline hover:underline text-blue-500 text-base my-4">
                   <Link
+                    className="text-orange font-semibold text-center bg-orange-400 px-4 py-2 m-2 rounded-full py-2 px-4 py-2 px-4 border-2 border-orange  my-8 "
                     to="/"
-                    className="no-underline hover:underline text-blue-500 text-base"
                   >
-                    Zabudli ste svoje heslo?
+                    Späť
                   </Link>
-                </div>
-                <div class="no-underline hover:underline text-blue-500 text-base">
-                  <Link
-                    to="/selectRegister"
-                    className="no-underline hover:underline text-blue-500 text-base"
+                  <button
+                    className="text-white font-semibold text-center bg-orange px-4 py-2 m-2 rounded-full py-2 px-4 my-8 "
+                    type="submit"
+                    disabled={submitting || pristine}
                   >
-                    Ešte nie ste zaregistrovaný? Vytvorte si účet TU.
-                  </Link>
+                    Prihlásiť
+                  </button>
+                  <div className="no-underline hover:underline text-blue-500 text-base my-4">
+                    <button
+                      type="submit"
+                      className="no-underline hover:underline text-blue-500 text-base"
+                      onClick={this.openModal}
+                    >
+                      Zabudli ste svoje heslo?
+                    </button>
+                    <Popup
+                      open={this.state.open}
+                      closeOnDocumentClick
+                      onClose={this.closeModal}
+                    >
+                      <div className="modal">
+                        <div className="font-bold text-4xl text-black flex flex-no-wrap mx-auto  justify-center mt-20 mb-10">
+                          Zabudli ste svoje heslo?
+                        </div>
+                        <div className="xl:flex xl:flex-no-wrap xl:mx-auto  xl:justify-center mb-5">
+                          <div className="mx-10">
+                            <div className="flex1 my-5 text-2xl text-black text-center">
+                              Zadajte svoju e-mailovú adresu a my
+                              <br />
+                              Vám pošleme link na obnovu Vášho hesla.
+                            </div>
+                            <div className="mt-10 ml-20">
+                              <Form
+                                onSubmit={this.onSubmitForgotPassword}
+                                render={({
+                                  handleSubmit,
+                                  form,
+                                  submitting,
+                                  pristine,
+                                  values,
+                                }) => (
+                                  <form onSubmit={handleSubmit}>
+                                    <FormTextField
+                                      name="email"
+                                      type="email"
+                                      text="Jozefdrabik@gmail.com"
+                                      className="bg-transparent  w-full text-gray-700  focus:outline-none"
+                                    />
+                                    <div className="flex flex-no-wrap mx-auto justify-center mb-10 mr-10">
+                                      <a
+                                        className="text-orange font-semibold text-center bg-orange-400 px-4 py-2 m-2 rounded-full py-2 px-4 py-2 px-4 border-2 border-orange w-1/2"
+                                        to="/"
+                                        onClick={this.closeModal}
+                                      >
+                                        Späť
+                                      </a>
+                                      <button
+                                        className="text-white font-semibold text-center bg-orange px-4 py-2 m-2 rounded-full py-2 px-4 w-1/2"
+                                        type="submit"
+                                        disabled={submitting || pristine}
+                                      >
+                                        Odoslať
+                                      </button>
+                                    </div>
+                                  </form>
+                                )}
+                              />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </Popup>
+                  </div>
+                  <div className="no-underline hover:underline text-blue-500 text-base">
+                    <Link
+                      to="/selectRegister"
+                      className="no-underline hover:underline text-blue-500 text-base"
+                    >
+                      Ešte nie ste zaregistrovaný? Vytvorte si účet TU.
+                    </Link>
+                  </div>
                 </div>
-              </div>
               </div>
               <div className="xl:w-6/12 backgroundLogin flex-1">
                 <div className="xl:mb-64">
--- src/pages/registerUser.jsx
+++ src/pages/registerUser.jsx
@@ -3,13 +3,30 @@ import authService from "../services/authService";
 import { Form } from "react-final-form";
 import FormTextField from "../components/common/formTextField";
 import { Link } from "react-router-dom";
-
+import PopupRegister from ".././components/popups/popupRegister";
 import ilustracia from "../staticImage/illustrations/Ilustracia_backgroundless3.png";
+import Popup from "reactjs-popup";
+import { NavLink } from "react-router-dom";
 
 class RegisterUser extends Component {
+  //popup
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+  openModal() {
+    this.setState({ open: true });
+  }
+  closeModal() {
+    this.setState({ open: false });
+  }
+
   onSubmit = async (values) => {
     await authService.registerUser(values);
-    this.props.history.push("/login");
+    //this.props.history.push("/login");
+    this.openModal = this.openModal.bind(this);
   };
 
   render() {
@@ -21,10 +38,10 @@ class RegisterUser extends Component {
             <div className="xl:ml-64 xl:flex mt-10">
               <div className="xl:w-6/12">
                 <div className="ml-32 ">
-                  <h1 className="text-left text-4xl font-bold pod mr-40 ">
+                  <h1 className="text-left text-4xl font-bold pod mr-40  ">
                     Registrovať sa
                   </h1>
-                  <p className="text-base mr-40">
+                  <p className="text-base mr-40 my-8">
                     Zaregistrujte sa pre uľahčenie hľadania
                     <br />
                     Vášho domáceho miláčika!
@@ -57,28 +74,73 @@ class RegisterUser extends Component {
                     type="password"
                     placeholder=""
                   />
-                  <div className="text-xs my-8 max-w-xs">
-                    Súhlasím so spracovaním emailovej adresy za účelom zaslania
-                    noviniek a ponúk emailom.
+                  <div>
+                    <div className="flex flex-wrap mt-5">
+                      <input
+                        type="radio"
+                        id="male"
+                        name="gender"
+                        value="male"
+                        className="xl:mr-2 w-1/8 xl:mt-1"
+                      />
+                      <div className="text-xs  max-w-xs w-7/8">
+                        Súhlasím so spracovaním emailovej adresy za účelom
+                        zaslania noviniek a ponúk emailom.
+                      </div>
+                    </div>
+
+                    <div className="text-sm my-8 max-w-xs">
+                      Ak budete pokračovať ďalej, vyjadrujete tak svoj súhlas s
+                      podmienkami používania, pravidlami ochrany súkromia a
+                      používaním súborov cookies.
+                    </div>
                   </div>
-                  <div className="text-sm my-8 max-w-xs">
-                    Ak budete pokračovať ďalej, vyjadrujete tak svoj súhlas s
-                    podmienkami používania, pravidlami ochrany súkromia a
-                    používaním súborov cookies.
-                  </div>
+                  
                   <Link
                     className="text-orange font-semibold text-center bg-orange-400 px-4 py-2 m-2 rounded-full py-2 px-4 py-2 px-4 border-2 border-orange content-end"
                     to="/selectRegister"
                   >
                     Späť
                   </Link>
+
                   <button
                     type="submit"
                     disabled={submitting || pristine}
                     className="text-white font-semibold text-center bg-orange px-4 py-2 m-2 rounded-full py-2 px-4"
+                    onClick={this.openModal}
                   >
                     Registrovať
                   </button>
+                  <Popup
+                    open={this.state.open}
+                    closeOnDocumentClick
+                    onClose={this.closeModal}
+                  >
+                    <div className="modal">
+                      <a className="close" onClick={this.closeModal}>
+                        &times;
+                      </a>
+                      <div className="font-bold text-4xl flex flex-no-wrap mx-auto  justify-center mt-20 mb-10">
+                        Práve Vám bol odoslaný potvrdzovací email
+                      </div>
+                      <div className="xl:flex xl:flex-no-wrap xl:mx-auto  xl:justify-center mb-5">
+                        <div className="mx-10">
+                          <button class="flex1 my-5 text-2xl">
+                            Pre dokončenie registrácie si prosím skontrolujte
+                            Vašu emailovú schránku a potvrďte email.
+                          </button>
+                        </div>
+                      </div>
+                      <div className="flex flex-no-wrap mx-auto justify-center mb-10">
+                        <NavLink
+                          className="text-white font-semibold text-center bg-orange px-4 py-2 m-2 rounded-full py-2 px-4"
+                          to="/"
+                        >
+                          Späť na stránku
+                        </NavLink>
+                      </div>
+                    </div>
+                  </Popup>
                   <div class="no-underline hover:underline text-blue-500 text-base my-4">
                     <Link
                       to="/login"

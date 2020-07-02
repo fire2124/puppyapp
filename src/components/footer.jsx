fea1497e6e05e9e887c6efa24b8383026d29b96a
fire2124 <filip.reichl@gmail.com>
2020-07-02 15:06:06 +0200

    responsive footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -15,9 +15,14 @@ const Footer = () => {
   }
 
   return (
-    <div>
-      <div className="md:flex md:items-center md:justify-start mx-auto py-10 px-10 space-x-1">
-        <div className="flex-1 px-4 py-2 m-2 w-1/6">
+    <div className="">
+      <div className="md:flex md:items-center md:justify-center md:mx-auto md:py-10 md:px-10 md:space-x-1 
+                      sm:justify-center sm:items-center sm:space-y-10  sm:mb-10 sm:mt-10">
+        <div className="xl:flex-1 xl:px-4 xl:py-2 xl:m-2 xl:w-1/6 xl:justify-left xl:text-left xl:mt-10
+                        lg:flex-1                        lg:w-1/6 lg:justify-left xl:text-left
+                        md:flex-1                        md:w-1/6 md:justify-left xl:text-left
+                        sm:flex-1                        sm:w-6/6 sm:justify-center sm:text-center
+                                                                                                ">
           <h1 className="text-black text-lg font-bold ">O nás</h1>
           <div className="no-underline hover:underline text-blue-500 text-base mt-1">
             Nájdite sa online
@@ -29,7 +34,11 @@ const Footer = () => {
             Partneri
           </div>
         </div>
-        <div className="flex-1 px-4 py-2 m-2 w-1/6">
+        <div className="xl:flex-1 xl:px-4 xl:py-2 xl:m-2 xl:w-1/6 xl:justify-left xl:text-left xl:mt-0
+                        lg:flex-1                        lg:w-1/6 lg:justify-left xl:text-left
+                        md:flex-1                        md:w-1/6 md:justify-left xl:text-left
+                        sm:flex-1                        sm:w-6/6 sm:justify-center sm:text-center
+                                                                                                ">
           <h1 className="text-black text-lg font-bold ">Útulky</h1>
           <div className="no-underline hover:underline text-blue-500 text-base mt-1">
             Ako adoptovať
@@ -38,20 +47,29 @@ const Footer = () => {
             Zoznam útulkov
           </div>
         </div>
-        <div className="flex-1 px-4 py-2 m-2 w-1/6">
+        <div className="xl:flex-1 xl:px-4 xl:py-2 xl:m-2 xl:w-1/6 xl:justify-left xl:text-left xl:mt-0
+                        lg:flex-1                        lg:w-1/6 lg:justify-left xl:text-left
+                        md:flex-1                        md:w-1/6 md:justify-left xl:text-left
+                        sm:flex-1                        sm:w-6/6 sm:justify-center sm:text-center
+                                                                                                ">
           <h1 className="text-black text-lg font-bold ">Sociálne siete</h1>
-          <div className="backgroundFooter1 w-1/6 float-left py-3"></div>
-          <div className="w-5/6 px-4 ml-3 no-underline hover:underline text-blue-500 text-base">
+          <div className="backgroundFooter1 xl:w-1/6 lg:w-1/6 md:w-1/6 float-left py-3 sm:ml-64 xl:ml-0 sm:mt-5 xl:mt-0"></div>
+          <div className="xl:w-5/6 lg:w-5/6 md:w-5/6 px-4 ml-3 no-underline  hover:underline text-blue-500 text-base sm:mr-64 xl:mr-0 sm:mt-5 xl:mt-0">
             Facebook
           </div>
         </div>
-        <div className="flex-1 px-4 py-2 m-2 w-3/6 mr-20">
+        <div className="xl:flex-1 xl:px-4 xl:py-2 xl:m-2 xl:w-3/6 xl:justify-left xl:text-left
+                        lg:flex-1                        lg:w-3/6 lg:justify-left xl:text-left
+                        md:flex-1                        md:w-3/6 md:justify-left xl:text-left
+                        sm:flex-1                        sm:w-6/6 sm:justify-center sm:text-center
+                                                                                                ">
           <h1 className="text-black text-2xl font-bold ">Newsletter</h1>
-          <div className="no-underline text-black-500 text-justify text-base mr-10 mt-1">
+          <div className="no-underline text-black-500 xl:text-justify xl:ml-0 text-base xl:mr-10 
+                                                       sm:text-center sm:ml-10 sm:mr-10">
             Začnite odoberať náš newsletter, aby ste boli informovaný o
             zbierkach a podujatiach, ktoré organizujú útulky.
           </div>
-          <div className="mt-5">
+          <div className="mt-5 sm:ml-32 sm:mr-32 xl:ml-0 xl:mr-0">
             <Form
               onSubmit={onSubmit}
               render={({
@@ -62,7 +80,7 @@ const Footer = () => {
                 values,
               }) => (
                 <form onSubmit={handleSubmit} className=" xl:mx-auto mt-10">
-                  <div className="w-11/12 border-b border-black my-4">
+                  <div className="xl:w-11/12 lg:w-11/12 md:w-11/12 border-b border-black my-4">
                     <Field
                       className="focus:outline-none my-2"
                       name="Odoslať"
@@ -80,14 +98,14 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="flex background3">
-        <div className=" flex-1 justify-center text-white text-center px-4 py-2 m-2">
+      <div className="xl:flex backgroundFooter3 ">
+        <div className=" xl:flex-1 xl:justify-center text-white text-center px-4 py-2 xl:m-2">
           Súbory cookies
         </div>
-        <div className="flex-1 justify-center text-white text-center px-4 py-2 m-2">
+        <div className="xl:flex-1 xl:justify-center text-white text-center px-4 py-2 xl:m-2">
           Ochrana osobných údajov
         </div>
-        <div className="flex-1 justify-center text-white text-center px-4 py-2 m-2">
+        <div className="xl:flex-1 xl:justify-center text-white text-center px-4 py-2 xl:m-2">
           Všeobecné podmienky
         </div>
       </div>

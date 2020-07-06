87847ae6316e756ae340283af06269b721c4c69b
fire2124 <filip.reichl@gmail.com>
2020-07-06 11:51:43 +0200

    add field type number

---
+++ build/precache-manifest.5df5306b69c11e510fff7b1230a8dca9.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d101e4a31c6f9386ce9c92ae7f470b8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "409e61def2edf3c51ffb",
+    "url": "/static/css/2.a04c2fe3.chunk.css"
+  },
+  {
+    "revision": "68f5ab76e5ac74e718d2",
+    "url": "/static/css/main.354b461e.chunk.css"
+  },
+  {
+    "revision": "409e61def2edf3c51ffb",
+    "url": "/static/js/2.a06d3001.chunk.js"
+  },
+  {
+    "revision": "47fab0d97a200128804c16ffd43a2bac",
+    "url": "/static/js/2.a06d3001.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68f5ab76e5ac74e718d2",
+    "url": "/static/js/main.060e6340.chunk.js"
+  },
+  {
+    "revision": "9bd7a91592a3826eb507",
+    "url": "/static/js/runtime-main.f3676756.js"
+  },
+  {
+    "revision": "5617da2e261f2afdb2c5ac2f9254ed4a",
+    "url": "/static/media/1920_peso (1).5617da2e.png"
+  },
+  {
+    "revision": "b436428135f66ecd96356fbc5e332f50",
+    "url": "/static/media/FINANCNA_POMOC-IMAGE.b4364281.png"
+  },
+  {
+    "revision": "01c44193d0158cb76c56c3bd18b52b2c",
+    "url": "/static/media/Group 165.01c44193.png"
+  },
+  {
+    "revision": "8e9db70863e618acd18d738867f49d7d",
+    "url": "/static/media/Group 166.8e9db708.png"
+  },
+  {
+    "revision": "4220655d17749be0103841a1f291a66e",
+    "url": "/static/media/Group 167.4220655d.png"
+  },
+  {
+    "revision": "d1d8c03afc6d435b8877d92b5a4481ac",
+    "url": "/static/media/Group 168.d1d8c03a.png"
+  },
+  {
+    "revision": "8b3d42d7c0de60e5a075f8d8f8e5c604",
+    "url": "/static/media/Group 169.8b3d42d7.png"
+  },
+  {
+    "revision": "7af5353f060335d7e8effae6e552b61e",
+    "url": "/static/media/Group 171.7af5353f.png"
+  },
+  {
+    "revision": "fd7f3edcf17fe8ac82289293f78a0de0",
+    "url": "/static/media/HAU-TO-IMAGE.fd7f3edc.png"
+  },
+  {
+    "revision": "9a4579b2cd5afe5ab4a89365ccaf8ef5",
+    "url": "/static/media/Ilustracia_backgroundless.9a4579b2.png"
+  },
+  {
+    "revision": "72d914036aa2c16337371ce0ef4e3cbc",
+    "url": "/static/media/Ilustracia_backgroundless2.72d91403.png"
+  },
+  {
+    "revision": "0dd05d22ebe502ca3c49cad3d87a01ee",
+    "url": "/static/media/Ilustracia_backgroundless3 – Register.0dd05d22.png"
+  },
+  {
+    "revision": "2a4edecd84adae66fd5918f7c6ddf7ab",
+    "url": "/static/media/Ilustracia_backgroundless3.2a4edecd.png"
+  },
+  {
+    "revision": "63aab5a047ddab54fe67297fdb346a43",
+    "url": "/static/media/Ilustracia_backgroundless4.63aab5a0.png"
+  },
+  {
+    "revision": "5ed8ac0103a130e5313bc2c47b7fdd87",
+    "url": "/static/media/Rectangle 213.5ed8ac01.png"
+  },
+  {
+    "revision": "daddaf9e79afdaa87d612a0cab27cd79",
+    "url": "/static/media/Untitled_Artwork 2.daddaf9e.png"
+  },
+  {
+    "revision": "c6746fc0214373d02f4095e867412eb4",
+    "url": "/static/media/brown-and-white-short-coated-puppy-1805164 (1).c6746fc0.png"
+  },
+  {
+    "revision": "1eae2e13a5bcc407cafe11cd66e1e5ee",
+    "url": "/static/media/brown-and-white-short-coated-puppy-1805164.1eae2e13.png"
+  },
+  {
+    "revision": "9bdd02ace777b4db1d5d4825713a7476",
+    "url": "/static/media/close-up-photography-of-adult-black-and-white-short-coat-dog-733416 (1).9bdd02ac.png"
+  },
+  {
+    "revision": "5a47addebcba2c30b023219a222f4ca0",
+    "url": "/static/media/close-up-photography-of-adult-black-and-white-short-coat-dog-733416.5a47adde.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5ecd491393c7bb53b077b3abcdb2f4df",
+    "url": "/static/media/registracia_utulok.5ecd4913.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "ef9968e50cedbb4721f0de681fd0de91",
+    "url": "/static/media/shallow 2.ef9968e5.png"
+  },
+  {
+    "revision": "f3fa15ebe81422d12a236a98b5c88fd3",
+    "url": "/static/media/shallow 3.f3fa15eb.png"
+  },
+  {
+    "revision": "68497212a0cbce87ee0b507bd3bba67d",
+    "url": "/static/media/shallow 4.68497212.png"
+  },
+  {
+    "revision": "9772a4bae364321466b8d120f6db1bbf",
+    "url": "/static/media/shallow 5.9772a4ba.png"
+  },
+  {
+    "revision": "70be819caf9ee203d269583c34f216ab",
+    "url": "/static/media/shallow-focus-photo-of-long-coated-dog-3361722 1.70be819c.png"
+  },
+  {
+    "revision": "f56ed15dcb4fe1b231bdb700ee938a6a",
+    "url": "/static/media/shel1.f56ed15d.png"
+  },
+  {
+    "revision": "7b4baa052a87794981a41a7b78bd1934",
+    "url": "/static/media/shel2.7b4baa05.png"
+  },
+  {
+    "revision": "c25927e31ca4d7390c6e80d11c4c9d51",
+    "url": "/static/media/shel3.c25927e3.png"
+  },
+  {
+    "revision": "d19aa6f11f57a3206277ae4742dab469",
+    "url": "/static/media/shellterImage.d19aa6f1.png"
+  }
+]);
---
+++ src/components/common/formNumberField.jsx
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import { Field } from "react-final-form";
+
+var iPlus = 0;
+var iMinus = 0;
+
+class FormNumberField extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      onPlus: 0,
+      onMinus: 0,
+      value: 0,
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handlePlus = this.handlePlus.bind(this);
+    this.handleMinus = this.handleMinus.bind(this);
+  }
+
+  handleInputChange() {
+    let val = this.state.onPlus + this.state.onMinus;
+    if (val <= 0) val = 0;
+    this.setState({value: val});
+    console.log("this is Handle:", this.state.value);
+
+  }
+
+  handlePlus() {
+    iPlus += 1;
+    this.setState({ onPlus: iPlus });
+    console.log(this.state.onPlus);
+  }
+  handleMinus() {
+    iMinus -= 1;
+    this.setState({ onMinus: iMinus });
+    console.log(this.state.onMinus);
+  }
+
+  render() {
+    return (
+      <div className="xl:flex xl:flex-no-wrap xl:justify-center mt-5 ">
+        <div className="justify-between input-group plus-minus-input">
+          <div className="input-group-button">
+            <button
+              name="firstMinus"
+              type="button"
+              className="button hollow circle fa fa-minus"
+              data-quantity="minus"
+              data-field="quantity"
+              onClick={this.handleMinus}
+            ></button>
+          </div>
+        </div>
+        <div className="justify-between">
+          <Field
+            name="numOfPeopleInHousehold"
+            component="input"
+            type="number"
+            step={1}
+            min={0}
+            className="w-10 border-b border-black text-center"
+            value={this.state.value}
+            onChange={this.handleInputChange}
+            
+          />
+        </div>
+        <div className="justify-between input-group plus-minus-input">
+          <div className="input-group-button flex1">
+            <button
+              type="button"
+              className="button hollow circle fa fa-plus"
+              data-quantity="plus"
+              data-field="quantity"
+              onClick={this.handlePlus}
+            ></button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default FormNumberField;
